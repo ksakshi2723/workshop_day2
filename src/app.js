702fa4cf36526abbde1991c524280656b8f4788a
kumarisakshi <kumarisakshi2723@gmail.com>
2021-07-20 19:22:33 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ const express = require("express")
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
-// const bycrypt =require("bycryptjs");
+const bcrypt=require('bcrypt');
 
+var jwt = require('jsonwebtoken');
+var cors = require('cors');
+
+app.use(cors());
 
 require("./db/conn");
 const Register = require("./models/registers");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 
@@ -27,27 +31,77 @@ app.get("/register", (req, res) => {
     res.render("register");
 })
 
+app.post('/login', async (req,res,next)=>{
+//    console.log(req.body);
+   const { email,password } = req.body;
+   console.log(req.body);
+ 
+
+    try {
+      let user = await Register.findOne({
+        email:req.body.email
+      });
+      if (!user)
+        return res.status(400).json({
+          message: "User Not Exist"
+        });
+
+    const isMatch=(password==user.password)
+    //   console.log(password,user.password);
+      if (!isMatch)
+        return res.status(400).json({
+          message: "Incorrect Password !"
+        });
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 3600
+        },
+        (err, token) => {
+          if (err) throw err;
+          
+          res.status(200).json({
+            token:token,
+            message:"login successfully"
+          });
+        }
+      );
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message: "Server Error"
+      });
+    }
+  
+})
+
+
 
 app.post("/register", async (req, res) => {
     try {
-        
+        console.log(req.body);
 
-        const password = req.body.pass;
-        const cpassword = req.body.cpassword;
+        const password = req.body.password;
+        const cpassword = req.body.confirmpassword;
         if (password === cpassword) {
-            const registeremp = new Register({
-                name: req.body.name,
-                password: password
-              
-            })
+            const registeremp = new Register(req.body);
          const token = await registeremp.generateAuthToken();
+         
          const registered = await registeremp.save();
-
-         res.send("register successfully !");
-            res.status(201).render(register)
+         console.log(token);
+         res.send({message:"register successfully !"});
+         res.status(201)
         }
         else {
-            res.send("password mis match");
+            res.send({message:"password mis match"});
         }
 
 
@@ -55,6 +109,7 @@ app.post("/register", async (req, res) => {
 
     catch (error) {
         res.status(400).send(error);
+        console.log("error")
     }
 })
 
--- src/models/registers.js
+++ src/models/registers.js
@@ -7,17 +7,37 @@ name:{
     type:String,
     required:true
 },
+email:{
+    type:String,
+    required:true,
+    unique:true
+},
+mobile:{
+    type:String,
+    required:true
+},
+
 password:{
     type:String,
     required:true
 },
+tokens:[{
+    token:{
+        type:String,
+    required:true
+    }
+}]
+
 
 })
 //generate token
 empSchema.methods.generateAuthToken=async function(){
     try{
+        console.log(this.id);
         const token=jwt.sign({_id:this._id.toString()},"acvbghnjmkloiuytredfgbhnkmlpo");
-        console.log(token);
+        this.tokens=this.tokens.concat({token:token})
+        await this.save();
+        return token;
     }
     catch (error){
         res.send("error part"+error);
